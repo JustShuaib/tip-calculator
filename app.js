9bb2487560aa234d4b53623b697734e439155f97
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-04-24 22:30:55 +0100

    Selected dom elements

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
+const billInput = document.getElementById("bill");
 const resetBtn = document.getElementById("reset-btn");
-console.log("Test");
-
-console.log(resetBtn.disabled);
+const customBtn = document.getElementById("custom");
+const peopleInput = document.getElementById("number");
+const tipOutput = document.getElementById("tip-amount");
+const totalOutput = document.getElementById("total-amount");
+const tipBtn = document.querySelectorAll(".btn");
+billInput.addEventListener("keyup", (e) => {
+  const bill = Number(e.target.value);
+  console.log(bill);
+});
+peopleInput.addEventListener("keyup", (e) => {
+  const people = Number(e.target.value);
+  console.log(people);
+});
+tipBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log(e.target.value);
+  });
+});
+customBtn.addEventListener("keyup", (e) => {
+  const bill = Number(e.target.value);
+  console.log(bill);
+});
