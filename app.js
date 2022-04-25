32b3395c96d06a299d169ae51ae2336e4242db91
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-04-25 15:46:12 +0100

    Pretty much done with the functionality

--- app.js
+++ app.js
@@ -5,71 +5,76 @@ const peopleInput = document.getElementById("number");
 const tipOutput = document.getElementById("tip-amount");
 const totalOutput = document.getElementById("total-amount");
 const tipBtn = document.querySelectorAll(".btn");
-const inputs = document.querySelectorAll("input");
 const errMsgs = document.querySelectorAll(".err--msg");
 const form = document.querySelector("form");
-let bill, tip, customTip, people;
-inputs.forEach((inputElement) => {
-  inputElement.addEventListener("change", () => {});
-});
 
-billInput.addEventListener("keyup", (e) => {
+// ***** GLOBAL VARS *******
+let bill, tip, customTip, people;
+
+//  ******** EVENT LISTENERS *********
+
+billInput.addEventListener("keyup", handleBill);
+peopleInput.addEventListener("keyup", handlePeople);
+tipBtn.forEach((btn) => btn.addEventListener("click", handleTip));
+customBtn.addEventListener("keyup", handleCustomTip);
+form.addEventListener("reset", resetState);
+
+//  ******** FUNCTIONS *********
+
+function handleBill(e) {
   bill = Number(e.target.value);
-  if (bill === 0) {
-    errMsgs[0].textContent = "Can't be zero";
-    billInput.classList.add("input--err");
-  } else {
-    errMsgs[0].textContent = "";
-    billInput.classList.remove("input--err");
-  }
-  resetBtn.disabled = false;
+  showErrorMessage(e, bill, 0);
   calcPerPerson();
   e.preventDefault();
-});
-peopleInput.addEventListener("keyup", (e) => {
+}
+function handlePeople(e) {
   people = Number(e.target.value);
-  if (people === 0) {
-    errMsgs[1].textContent = "Can't be zero";
-    peopleInput.classList.add("input--err");
-  } else {
-    errMsgs[1].textContent = "";
-    peopleInput.classList.remove("input--err");
+  showErrorMessage(e, people, 1);
+  calcPerPerson();
+  e.preventDefault();
+}
+function showErrorMessage(event, prop, index) {
+  if (prop <= 0) {
+    errMsgs[index].textContent = `Can't be ${prop === 0 ? "zero" : "negative"}`;
+    event.target.classList.add("input--err");
+    return;
   }
+  errMsgs[index].textContent = "";
+  event.target.classList.remove("input--err");
+  resetBtn.disabled = false;
+}
+function handleTip(e) {
+  tipBtn.forEach((btn) => btn.classList.remove("btn--active"));
+  e.target.classList.add("btn--active");
+  tip = Number(e.target.value);
+  customBtn.value = "";
   resetBtn.disabled = false;
   calcPerPerson();
   e.preventDefault();
-});
-tipBtn.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    tipBtn.forEach((btn) => btn.classList.remove("btn--active"));
-    btn.classList.add("btn--active");
-    tip = Number(btn.value);
-    customBtn.value = "";
-    resetBtn.disabled = false;
-    calcPerPerson();
-    e.preventDefault();
-  });
-});
-customBtn.addEventListener("keyup", (e) => {
+}
+function handleCustomTip(e) {
   customTip = Number(e.target.value) / 100;
   if (customTip > 0) {
+    tipBtn.forEach((btn) => btn.classList.remove("btn--active"));
     resetBtn.disabled = false;
     tip = customTip;
     calcPerPerson();
   }
-});
+}
+
 function calcPerPerson() {
   const totalPerPerson = (bill * (1 + tip)) / people;
   const tipPerPerson = (bill * tip) / people;
-  if (isFinite(totalPerPerson) && isFinite(tipPerPerson)) {
+  if (totalPerPerson > 0 && tipPerPerson > 0) {
     tipOutput.textContent = `$${tipPerPerson.toFixed(2)}`;
     totalOutput.textContent = `$${totalPerPerson.toFixed(2)}`;
   }
 }
-form.addEventListener("reset", () => {
+
+function resetState() {
   resetBtn.disabled = true;
   tipBtn.forEach((btn) => btn.classList.remove("btn--active"));
   (bill = 0), (tip = 0), (customTip = 0), (people = 0);
   tipOutput.textContent = "$0.00";
   totalOutput.textContent = "$0.00";
-});
+}
